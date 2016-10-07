73b0c711e21b54dad8bd698b810f67c95c8ec409
mamuira <iramamu302@gmail.com>
2016-10-07 16:55:33 +0900

    Add password resets & email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
-  before_save :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness:{case_sensitive: false }
   has_secure_password
@@ -37,6 +38,18 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   # 渡されたトークンがダイジェストと一致したらtrueを返す
   # トークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
@@ -48,6 +61,11 @@ class User < ApplicationRecord
   # ユーザーログインを破棄する
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
